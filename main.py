21b8547e5655ace4b9dea92903393a53ab844a60
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-05-11 02:36:26 +0000

    Fixed.

--- main.py
+++ main.py
@@ -99,11 +99,11 @@ def display_metrics(interview_data):
 
 
 
-# Function to clear interview data
-""" def clear_data():
-    if st.button("🗑️ Clear Data", key="clear_button"):
-        st.session_state.interview_data = pd.DataFrame(columns=["Name", "Position", "Status", "Interviewer Name", "Date of Interview", "Round", "Interview Questions", "Interview Answers"])
-        save_data_to_csv(st.session_state.interview_data) """
+# # Function to clear interview data
+# """ def clear_data():
+#     if st.button("🗑️ Clear Data", key="clear_button"):
+#         st.session_state.interview_data = pd.DataFrame(columns=["Name", "Position", "Status", "Interviewer Name", "Date of Interview", "Round", "Interview Questions", "Interview Answers"])
+#         save_data_to_csv(st.session_state.interview_data) """
 
 # Function to download interview data as CSV
 def download_data(interview_data):
